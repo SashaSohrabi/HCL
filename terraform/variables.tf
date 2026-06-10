60c0e239f703421f025568ab299903b036bb48b8
sasha sohrabi <sasha.sohrabi@schule-zukunftsmotor.org>
2026-06-10 16:18:26 +0200

    hands-on lab 4

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,13 +10,13 @@ variable "environment" {
   default     = "learning-terraform"
 }
 
-variable "public_subnet_cidrs" {
+variable "public_subnet_cidr" {
   description = "CIDR block for public subnet"
   type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "private_subnet_cidrs" {
+variable "private_subnet_cidr" {
   description = "CIDR block for private subnet"
   type        = string
   default     = "10.0.2.0/24"
